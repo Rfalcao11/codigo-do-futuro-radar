9325258ac4dec2fd991f9a96e6e37aad0b1b4357
Gustavo Pereira <you@example.com>
2022-11-29 16:29:12 -0300

    rotas home e pedidos

--- desafio-angular/src/app/app-routing.module.ts
+++ desafio-angular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { PedidosComponent } from './views/pedidos/pedidos.component';
 
 const routes: Routes = [
   {path: 'pedidos', component: PedidosComponent},
+  {path:'', component: HomeComponent}
 
 ];
 
