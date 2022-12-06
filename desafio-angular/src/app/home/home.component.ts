953cd482d5e070064656bb7c396d2da797f48b8d
jazielmatos <jazielmatos82@gmail.com>
2022-12-05 23:39:01 -0300

    Mechendo com o charts

--- desafio-angular/src/app/home/home.component.ts
+++ desafio-angular/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { CurrencyPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -30,6 +31,10 @@ export class HomeComponent implements OnInit {
   private pedidoProdutoServico:PedidoProdutoServico = {} as PedidoProdutoServico
   private produtoServico:ProdutoServico = {} as ProdutoServico
 
+  
+  public valTotal:number = 0
+  public qtdClientes:number | any
+
 
 // declarar dados
   public pedido:Pedido | undefined = {} as Pedido
@@ -38,23 +43,72 @@ export class HomeComponent implements OnInit {
   public produtos:Produto[] | any = [] 
   public pedidosProdutos:any = []
   public pedidos:any = []
+  public todosClientes:any = []
 
   //TIPO CHARTS
   bar:ChartType = ChartType.Bar
   pedidosPorMedicamento:ChartType = ChartType.PieChart
+  chartGraficoData:ChartType = ChartType.GeoChart
+  graficoColuna:ChartType = ChartType.ColumnChart
   ///////////////////////////////////////////
+  
+  //DADOS CHARTS
   public meusDados:any[] = [ ];
-  public dadosMedicamento:any = [ ];
+  public dadosMedicamento:any = [ 
+    
+  ];
+  public dadosGraficoData:any = [ ]
 
-  public valTotal:number = 0
-  public qtdClientes:number | any
+  public dataGraficoColuna = [
+    ['Move', 23],
+    ["King's pawn (e4)", 44],
+    ["Queen's pawn (d4)", 31],
+    ["Knight to King 3 (Nf3)", 12],
+    ["Queen's bishop pawn (c4)", 10],
+    ['Other', 3]
 
+  ]
+  
+  ////////////////////////////////////
+
+
+  //OPTIONS DOS CHARTS
+  public optionsColuna = {
+    width: 800,
+    legend: { position: 'none' },
+    chart: {
+      title: 'Chess opening moves',
+      subtitle: 'popularity by percentage' },
+    axes: {
+      x: {
+        0: { side: 'top', label: 'White to move'} // Top x-axis.
+      }
+    },
+    bar: { groupWidth: "90%" }
+  };
+
+  public geoChartsOption = {
+    region: 'BR',
+    resolution: 'provinces',
+    width: 900,
+    height: 300,
+    colorAxis: {
+      colors: ['#EAF2F8','#D4E6F1', '#A9CCE3','#7FB3D5', '#5499C7','#2980B9','#2471A3','#1F618D','#1A5276', '#154360']
+  }
+  
+}
+
+
+  public optionsProdutos = {
+    pieSliceText: 'label',
+    legend: 'none',
+
+  }
   public optionsMedicamento = {
-    title: 'Medicamentos mais Vendidos',
+    pieSliceText: 'none',
     pieHole: 0.4
   }
-
-
+  /////////////////////////////////////////////////
 
   ngOnInit(): void {
     this.pedidoServico = new PedidoServico(this.http)
@@ -71,36 +125,82 @@ export class HomeComponent implements OnInit {
     this.pedidosProdutos = await this.pedidoProdutoServico.listarPedidoProduto()
     this.pedidos = await this.pedidoServico.listarPedidos()
     this.qtdClientes = await this.clienteServico.listarTamanhoClientes()
+    this.todosClientes = await this.clienteServico.listarClientes()
+
+    //DADOS PRODUTOS - ESTOQUE
     let listaNovaProdutos = []
     for(let i = 0; i<this.produtos.length;i++){
       listaNovaProdutos.push([this.produtos[i].nome,this.produtos[i].qtdEstoque])
     }
     this.meusDados = listaNovaProdutos
+    /* ------------------------------------------------------------------------------*/
 
+    //DADOS GANHO POR PEDIDOS
     let listaPedidosProdutosNova = []
     for(let i =0;i<this.pedidosProdutos.length;i++){
       listaPedidosProdutosNova.push( [this.produtos[(this.pedidosProdutos[i].idProduto)-1].nome, this.pedidosProdutos[i].valorTotal])
     }
-
-    for(let i=0;i<this.pedidos.length;i++){
-      this.valTotal += this.pedidos[i].valorTotal
-    }
-
-
-    let listaPedidoProdutoOficial =[]
-    for(let pedidoProd of listaPedidosProdutosNova){
-      for(let o=0;o<pedidoProd.length;o++){
-        let nome = pedidoProd[o]
-        
+    let filtrado:any = {}
+    for(let i=0; i< listaPedidosProdutosNova.length; i++){
+      let dado = listaPedidosProdutosNova[i]
+      if(filtrado[dado[0]]) {
+         filtrado[dado[0]] += dado[1]
+      }
+      else{
+         filtrado[dado[0]] = dado[1]
       }
     }
+    let chaves = Object.keys(filtrado)
+    let novo = []
+    for(let i=0; i< chaves.length; i++){
+      let key = chaves[i]
+      let valor = filtrado[key]
+      novo.push([key, valor])   
 
+    }
+    this.dadosMedicamento = novo
+    /* --------------------------------------------------------------------------------------------------*/
+
+
+    //DADO GANHO TOTAL
+    for(let i=0;i<this.pedidos.length;i++){
+      this.valTotal += this.pedidos[i].valorTotal
+    } 
+    /* --------------------------------------------------------------------------------------------------*/
+
+
+    //DADO GRAFICO GEOGRAFICO - CALOR
+    let listaClientesPorEstado = []
+    for(let i=0;i<this.todosClientes.length;i++){
+      listaClientesPorEstado.push([`BR-${this.todosClientes[i].estado}`, 1])
+    }
+    let Estadofiltrado:any = {}
+    for(let i=0; i< listaClientesPorEstado.length; i++){
+      let dado = listaClientesPorEstado[i]
+      if(Estadofiltrado[dado[0]]) {
+        Estadofiltrado[dado[0]] += dado[1]
+      }
+      else{
+        Estadofiltrado[dado[0]] = dado[1]
+      }
+    }
+    let chavesCliente = Object.keys(Estadofiltrado)
+    let novoDado = []
+    for(let i=0; i< chavesCliente.length; i++){
+      let key = chavesCliente[i]
+      let valor = Estadofiltrado[key]
+      novoDado.push([key, valor])   
+
+    }
+    this.dadosGraficoData = novoDado
+    console.log("Lista estado",this.dadosGraficoData)
     
-    console.log(this.pedidos)
-    this.dadosMedicamento = listaPedidosProdutosNova
-    console.log(this.pedidosProdutos)
-    console.log(listaNovaProdutos)
 
+    // console.log(listaPedidosProdutosNova)
+    // console.log(this.pedidos)
+    // console.log(this.pedidosProdutos)
+    // console.log(listaNovaProdutos)
+    
   }
 
   
